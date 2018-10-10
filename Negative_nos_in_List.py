b279eec57fe4b30fbd91892dc3bc13968bcc08ae
Arun Sengar <lokmsingh@gmail.com>
2018-10-10 21:52:55 +0530

    My second commit

---
+++ Negative_nos_in_List.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 10 21:15:50 2018
+
+@author: Arun
+"""
+
+#### Write a Program to receive only the negative numbers from a list
+
+list = [2,46,445,-2,-4,-6,-33]
+list2 = []
+
+for x in list:
+    if (x >= 0):
+        pass
+    else:
+        list2.append(x)
+print(list2)
+        
+
