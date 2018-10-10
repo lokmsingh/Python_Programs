a6b172515ec1569003fa0684814ad7363df5af46
Arun Sengar <lokmsingh@gmail.com>
2018-10-10 22:34:17 +0530

    My third program

---
+++ Command line arguments.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 10 21:48:27 2018
+
+@author: Arun
+"""
+
+### Accept two numbers from the command line and find their sum
+import sys
+a = float (sys.argv[1])
+b = float(sys.argv[2])
+c = a+b
+print('Sum=',c)
+
+
+### Run the Program through command Line using---  python Command line arguments.py 10 20
